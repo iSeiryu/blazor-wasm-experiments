9f54164f5edc172f6f8d09ff304866535f034714
iSeiryu <iseiryu@gmail.com>
2022-02-07 15:49:05 -0500

    Converted the app to dotnet 6.
    Former-commit-id: 55e3f9a92716d331e5a823092afcf4ab4c3eba22 [formerly ad92e9696772de7307503121cf79c6636c019066]
    Former-commit-id: 0494c01664f75a333cecf188077860af36e4612c
    Former-commit-id: 99f3fddd3b4421b1c50b9f2d0872bdf171e889e9
    Former-commit-id: c80166bf76646432998e40bc0cfa221a63ea1b27
    Former-commit-id: a77ef88062767439ee9ae9f262e01dc10432b39c

--- BlazorTests/Program.cs
+++ BlazorTests/Program.cs
@@ -1,20 +1,14 @@
 using System;
 using System.Net.Http;
-using System.Threading.Tasks;
 using BlazorStyled;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using BlazorTests;
 
-namespace BlazorTests {
-    public class Program {
-        public static async Task Main(string[] args) {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.Services.AddBlazorStyled();
-            builder.RootComponents.Add<App>("#app");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddBlazorStyled();
+builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
