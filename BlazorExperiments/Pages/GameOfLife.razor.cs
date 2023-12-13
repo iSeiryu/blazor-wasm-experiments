7a6f55dc3cda56ecc60225dcc924fd4a0724a66a
iSeiryu <iseiryu@gmail.com>
2023-12-13 14:22:26 -0500

    GameOfLife: optimize memory usage and rendering; add Generation indicator.
    Former-commit-id: 9ff86dd47eb3304a25e4b86855cfa36625f7761a [formerly f99c98ec3e328341e5b6f8e1993dfc79681a10c1]
    Former-commit-id: 0b4dc5c79e06481e345fe5dc2f7c27b718185f07
    Former-commit-id: c574b2b1bb12af554a68b77a7a67d495f275af08
    Former-commit-id: 83bdc2d84505329feface4f824175b0ec1d476d4
    Former-commit-id: 63017fedfcfcef7942b130fd6c4c71e41cbff5c2

--- BlazorExperiments/Pages/GameOfLife.razor.cs
+++ BlazorExperiments/Pages/GameOfLife.razor.cs
@@ -5,9 +5,10 @@ using Excubo.Blazor.Canvas.Contexts;
 namespace BlazorExperiments.UI.Pages;
 public partial class GameOfLife {
     CanvasComponent _canvas = null!;
-    int[,] _board = null!;
+    bool[,] _board = null!;
     int _rows = 0;
     int _cols = 0;
+    long _generation = 0;
     const int CellSize = 10;
     DateTime _lastTime = DateTime.Now;
     TimeSpan _refreshSpeedInMilliseconds = TimeSpan.Zero;
@@ -16,22 +17,24 @@ public partial class GameOfLife {
     void Initialize() {
         _rows = (int)_canvas.Height / CellSize;
         _cols = (int)_canvas.Width / CellSize;
-        _board = new int[_rows, _cols];
+        _board = new bool[_rows, _cols];
+        _generation = 0;
         StateHasChanged();
 
-        AddGliderShape(_board);
-        AddExploderShape(_board, 10, 0);
-        AddStableShape(_board, 10, 10);
+        AddGliderShape(ref _board);
+        AddExploderShape(ref _board, 10, 0);
+        AddStableShape(ref _board, 10, 10);
         if (_cols < 35) {
-            AddPulsarShape(_board, 20, 10);
-            AddTetrominoShape(_board, 35, 20);
+            AddPulsarShape(ref _board, 20, 10);
+            AddTetrominoShape(ref _board, 35, 20);
         }
         else {
-            AddPulsarShape(_board, 20, 20);
-            AddTetrominoShape(_board, 10, 30);
+            AddPulsarShape(ref _board, 20, 20);
+            AddTetrominoShape(ref _board, 10, 30);
         }
 
         _refreshSpeedInMilliseconds = TimeSpan.FromMilliseconds(_milliSeconds);
+        _canvas.Timer.Enabled = true;
 
         StateHasChanged();
     }
@@ -44,36 +47,41 @@ public partial class GameOfLife {
 
         await using var batch = _canvas.Context.CreateBatch();
         await ClearScreenAsync(batch);
-        int[,] newBoard = new int[_rows, _cols];
+        await batch.FillStyleAsync("white");
+
+        var newBoard = new bool[_rows, _cols];
+        var boardHasChanged = false;
+        _generation++;
 
         for (int row = 0; row < _rows; row++) {
             for (int col = 0; col < _cols; col++) {
-                int liveNeighbors = 0;
-                liveNeighbors = GetLiveNeighbors(col, row);
+                int liveNeighbors = GetLiveNeighbors(col, row);
 
-                if (_board[row, col] == 1 && (liveNeighbors < 2 || liveNeighbors > 3)) {
-                    newBoard[row, col] = 0;
+                if (_board[row, col] && (liveNeighbors < 2 || liveNeighbors > 3)) {
+                    boardHasChanged = true;
+                    newBoard[row, col] = false;
                 }
-                else if (_board[row, col] == 0 && liveNeighbors == 3) {
-                    newBoard[row, col] = 1;
+                else if (!_board[row, col] && liveNeighbors == 3) {
+                    boardHasChanged = true;
+                    newBoard[row, col] = true;
                 }
                 else {
                     newBoard[row, col] = _board[row, col];
                 }
 
-                if (newBoard[row, col] == 1) {
-                    await batch.FillStyleAsync("white");
-                }
-                else {
-                    await batch.FillStyleAsync("black");
+                if (newBoard[row, col]) {
+                    await batch.FillRectAsync(col * CellSize, row * CellSize, CellSize, CellSize);
                 }
 
-                await batch.FillRectAsync(col * CellSize, row * CellSize, CellSize, CellSize);
             }
         }
         _board = newBoard;
+        if (!boardHasChanged)
+            Pause();
+
+        StateHasChanged();
     }
-    private int GetLiveNeighbors(int x, int y) {
+    int GetLiveNeighbors(int x, int y) {
         int liveNeighbors = 0;
 
         // Check the 8 surrounding cells
@@ -87,14 +95,18 @@ public partial class GameOfLife {
                 int newCol = x + col;
 
                 // Check if the coordinates are within the board boundaries
-                if (newRow >= 0 && newRow < _board.GetLength(0) && newCol >= 0 && newCol < _board.GetLength(1)) {
+                if (newRow >= 0 && newRow < _rows && newCol >= 0 && newCol < _cols) {
                     // If the neighbor is alive, increment the count
-                    if (_board[newRow, newCol] == 1)
+                    if (_board[newRow, newCol])
                         liveNeighbors++;
                 }
             }
         }
         return liveNeighbors;
+    }
+
+    void Pause() {
+        _canvas.Timer.Enabled = false;
     }
 
     async Task ClearScreenAsync(Batch2D batch) {
@@ -103,72 +115,72 @@ public partial class GameOfLife {
         await batch.FillRectAsync(0, 0, _canvas.Width, _canvas.Height);
     }
 
-    void AddGliderShape(int[,] board) {
-        board[1, 1] = 1;
-        board[1, 2] = 1;
-        board[1, 3] = 1;
-        board[2, 1] = 1;
-        board[3, 2] = 1;
+    void AddGliderShape(ref bool[,] board) {
+        board[1, 1] = true;
+        board[1, 2] = true;
+        board[1, 3] = true;
+        board[2, 1] = true;
+        board[3, 2] = true;
     }
 
-    void AddExploderShape(int[,] board, int row = 0, int col = 0) {
-        board[row, col + 2] = 1;
-        board[row + 1, col] = 1;
-        board[row + 1, col + 2] = 1;
-        board[row + 2, col] = 1;
-        board[row + 2, col + 1] = 1;
-        board[row + 2, col + 2] = 1;
-        board[row + 3, col + 2] = 1;
+    void AddExploderShape(ref bool[,] board, int row = 0, int col = 0) {
+        board[row, col + 2] = true;
+        board[row + 1, col] = true;
+        board[row + 1, col + 2] = true;
+        board[row + 2, col] = true;
+        board[row + 2, col + 1] = true;
+        board[row + 2, col + 2] = true;
+        board[row + 3, col + 2] = true;
     }
 
-    void AddStableShape(int[,] board, int row = 0, int col = 0) {
-        board[row, col] = 1;
-        board[row + 1, col] = 1;
-        board[row, col + 1] = 1;
-        board[row, col + 2] = 1;
+    void AddStableShape(ref bool[,] board, int row = 0, int col = 0) {
+        board[row, col] = true;
+        board[row + 1, col] = true;
+        board[row, col + 1] = true;
+        board[row, col + 2] = true;
     }
 
-    void AddTetrominoShape(int[,] board, int row = 0, int col = 0) {
-        board[row, col] = 1;
-        board[row, col + 1] = 1;
-        board[row, col + 2] = 1;
-        board[row - 1, col + 1] = 1;
+    void AddTetrominoShape(ref bool[,] board, int row = 0, int col = 0) {
+        board[row, col] = true;
+        board[row, col + 1] = true;
+        board[row, col + 2] = true;
+        board[row - 1, col + 1] = true;
     }
 
-    void AddPulsarShape(int[,] board, int row = 0, int col = 0) {
-        board[row, col + 2] = 1;
-        board[row, col + 3] = 1;
-        board[row, col + 4] = 1;
-        board[row, col + 8] = 1;
-        board[row, col + 9] = 1;
-        board[row, col + 10] = 1;
-        board[row + 2, col] = 1;
-        board[row + 2, col + 5] = 1;
-        board[row + 2, col + 7] = 1;
-        board[row + 2, col + 12] = 1;
-        board[row + 3, col] = 1;
-        board[row + 3, col + 5] = 1;
-        board[row + 3, col + 7] = 1;
-        board[row + 3, col + 12] = 1;
-        board[row + 4, col] = 1;
-        board[row + 4, col + 5] = 1;
-        board[row + 4, col + 7] = 1;
-        board[row + 4, col + 12] = 1;
-        board[row + 5, col + 2] = 1;
-        board[row + 5, col + 3] = 1;
-        board[row + 5, col + 4] = 1;
-        board[row + 5, col + 8] = 1;
-        board[row + 5, col + 9] = 1;
-        board[row + 5, col + 10] = 1;
-        board[row + 7, col + 2] = 1;
-        board[row + 7, col + 3] = 1;
-        board[row + 7, col + 4] = 1;
-        board[row + 7, col + 8] = 1;
-        board[row + 7, col + 9] = 1;
-        board[row + 7, col + 10] = 1;
-        board[row + 8, col] = 1;
-        board[row + 8, col + 5] = 1;
-        board[row + 8, col + 7] = 1;
-        board[row + 8, col + 12] = 1;
+    void AddPulsarShape(ref bool[,] board, int row = 0, int col = 0) {
+        board[row, col + 2] = true;
+        board[row, col + 3] = true;
+        board[row, col + 4] = true;
+        board[row, col + 8] = true;
+        board[row, col + 9] = true;
+        board[row, col + 10] = true;
+        board[row + 2, col] = true;
+        board[row + 2, col + 5] = true;
+        board[row + 2, col + 7] = true;
+        board[row + 2, col + 12] = true;
+        board[row + 3, col] = true;
+        board[row + 3, col + 5] = true;
+        board[row + 3, col + 7] = true;
+        board[row + 3, col + 12] = true;
+        board[row + 4, col] = true;
+        board[row + 4, col + 5] = true;
+        board[row + 4, col + 7] = true;
+        board[row + 4, col + 12] = true;
+        board[row + 5, col + 2] = true;
+        board[row + 5, col + 3] = true;
+        board[row + 5, col + 4] = true;
+        board[row + 5, col + 8] = true;
+        board[row + 5, col + 9] = true;
+        board[row + 5, col + 10] = true;
+        board[row + 7, col + 2] = true;
+        board[row + 7, col + 3] = true;
+        board[row + 7, col + 4] = true;
+        board[row + 7, col + 8] = true;
+        board[row + 7, col + 9] = true;
+        board[row + 7, col + 10] = true;
+        board[row + 8, col] = true;
+        board[row + 8, col + 5] = true;
+        board[row + 8, col + 7] = true;
+        board[row + 8, col + 12] = true;
     }
 }
